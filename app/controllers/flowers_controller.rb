7dfe268bfa57ebbf1015ff142092ccd5734b7713
Chisato Murakami <kiwipom1031@gmail.com>
2020-06-16 21:44:20 -0700

    add index, new, create actions and view

--- app/controllers/flowers_controller.rb
+++ app/controllers/flowers_controller.rb
@@ -2,4 +2,25 @@ class FlowersController < ApplicationController
   def home
     render :home
   end
+
+  def index
+    @flowers = Flower.all
+    render :index
+  end
+
+  def new
+    @flowers = Flower.new
+    render :new
+  end
+
+  def create
+    @flower = Flower.create!(flower_params)
+    redirect_to flowers_path
+  end
+
+  private
+  def flower_params
+    params.require(:flower).permit(:title)
+  end
+
 end
