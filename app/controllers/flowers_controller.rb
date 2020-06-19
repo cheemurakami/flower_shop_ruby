e4f686f6642aa75778383d1d9fbf3be4568e2785
Chisato Murakami <kiwipom1031@gmail.com>
2020-06-19 16:05:13 -0700

    add edit and destroy controller

--- app/controllers/flowers_controller.rb
+++ app/controllers/flowers_controller.rb
@@ -18,6 +18,17 @@ class FlowersController < ApplicationController
     redirect_to flowers_path
   end
 
+  def edit
+    @flower = Flower.find(params[:id])
+    render :edit
+  end
+
+  def destroy
+    @flower = Flower.find(params[:id])
+    @flower.destroy   
+    redirect_to flowers_path
+  end
+
   private
   def flower_params
     params.require(:flower).permit(:title)
